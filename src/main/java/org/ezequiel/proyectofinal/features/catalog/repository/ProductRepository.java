1306f3138ed5d6a961e7bc306326627e4841e3ed
Ezequiel Alarcon <ps3ezequiel@gmail.com>
2026-03-28 14:43:18 +0100

    solving problem os query N+1 in ProductRepository

--- src/main/java/org/ezequiel/proyectofinal/features/catalog/repository/ProductRepository.java
+++ src/main/java/org/ezequiel/proyectofinal/features/catalog/repository/ProductRepository.java
@@ -1,9 +1,15 @@
 package org.ezequiel.proyectofinal.features.catalog.repository;
 
 import org.ezequiel.proyectofinal.features.catalog.entity.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Short> {
+
+    @EntityGraph(attributePaths = {"category", "supplier"})
+    List<Product> findAll();
 }
